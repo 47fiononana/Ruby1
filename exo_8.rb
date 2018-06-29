a5e77eba3774711e96b86ba44530332f0e67f40f
47fiononana <rantofinonanafanomezana@gmail.com>
2018-06-29 08:07:32 +0300

    Ruby

---
+++ exo_8.rb
@@ -0,0 +1,4 @@
+puts "Bonjour,"
+print "quel est ton nom"
+user_name = gets.chomp
+puts "ton nom est #{user_name}"
---
+++ exo_9.rb
@@ -0,0 +1,4 @@
+puts "bonjour,"
+print "quel est votre prenom"
+user_name = gets.chomp
+puts"ton nom est #{user_name}" 
